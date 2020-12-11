8cc04506133830ee8927ba6d936518ee6046ef9f
sunji <sung1011@gmail.com>
2020-12-11 18:27:22 +0800

    "ig:"

--- init.sh
+++ init.sh
@@ -6,8 +6,6 @@ CRACK_PATH="/usr/local/crack/"
 CRACK_PKG_PATH="/usr/local/crack/pkg/"
 # bin目录
 CRACK_BIN_PATH="/usr/local/bin/"
-# go version
-GO_VER="1.15.6"
 
 # 创建目录
 mkdir -p "$CRACK_PATH"
@@ -15,13 +13,16 @@ mkdir -p "$CRACK_PKG_PATH"
 
 # -------- go
 
-# download golang
-curl -sL https://golang.org/dl/go"$GO_VER".linux-amd64.tar.gz > /tmp/go"$GO_VER".tar.gz
-# install golang
-tar -zxvf /tmp/go"$GO_VER".tar.gz -C "$CRACK_PATH"
-# link bin
-ln -s "$CRACK_PATH"go/bin/go "$CRACK_BIN_PATH"
-
+# go version
+if [ ! -x go ]; then
+    GO_VER="1.15.6"
+    # download golang
+    curl -sL https://golang.org/dl/go"$GO_VER".linux-amd64.tar.gz > /tmp/go"$GO_VER".tar.gz
+    # install golang
+    tar -zxvf /tmp/go"$GO_VER".tar.gz -C "$CRACK_PATH"
+    # link bin
+    ln -s "$CRACK_PATH"go/bin/go "$CRACK_BIN_PATH"
+fi
 # -------- git
 
 # install
